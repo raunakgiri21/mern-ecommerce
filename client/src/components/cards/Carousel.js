48d8ff4c9d55621f626c2c59f38c37c3691c33ce
Raunak <raunakgiri21@gmail.com>
2023-03-17 21:30:24 +0530

    worked on ProductView page

--- client/src/components/cards/Carousel.js
+++ client/src/components/cards/Carousel.js
@@ -1,19 +1,19 @@
-import { Carousel } from 'antd';
+import Slider from "react-slick";
 import carousel01 from '../../assets/images/carousel/1.jpg'
 import carousel02 from '../../assets/images/carousel/2.jpg'
 import carousel03 from '../../assets/images/carousel/3.jpg'
 import carousel04 from '../../assets/images/carousel/4.jpg'
-const contentStyle = {
-  height: '206px',
-  color: '#fff',
-  maxWidth: '100%',
-  width: 'auto',
-  margin:'auto',
-  display:'block'
+const settings = {
+  dots: false,
+  autplay: true,
+  infinite: true,
+  speed: 400,
+  slidesToShow: 1,
+  slidesToScroll: 1,
 };
 const CarouselCard = () => (
-  <Carousel autoplay>
-    <div style={contentStyle}>
+  <Slider autoplay {...settings}>
+    <div>
       <img src={carousel02} className='w-100 h-100 object-fit-cover'/>
     </div>
     <div>
@@ -25,6 +25,6 @@ const CarouselCard = () => (
     <div>
       <img src={carousel04} className='w-100 h-100 object-fit-cover'/>
     </div>
-  </Carousel>
+  </Slider>
 );
 export default CarouselCard;
