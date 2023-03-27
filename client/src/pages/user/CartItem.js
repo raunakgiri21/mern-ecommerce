ea2f03a62b4c4f66254775219b10eb720061f409
Raunak <raunakgiri21@gmail.com>
2023-03-27 21:01:41 +0530

    Update CartItem Quantity from Cart

--- client/src/pages/user/CartItem.js
+++ client/src/pages/user/CartItem.js
@@ -3,10 +3,12 @@ import axios from 'axios';
 import { useAuth } from '../../context/auth';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
+import { InputNumber } from 'antd';
 
 const CartItem = ({item, setTotalPrice, setChange}) => {
     const [product, setProduct] = useState({});
     const [auth, setAuth] = useAuth();
+    const [quantity,setQuantity] = useState(1);
     const navigate = useNavigate()
     const location = useLocation()
 
@@ -18,6 +20,7 @@ const CartItem = ({item, setTotalPrice, setChange}) => {
         try {
             const {data} = await axios.get(`product/productID/${item.productID}`)
             setProduct(data)
+            setQuantity(item.quantity)
             setTotalPrice(prevPrice => prevPrice+data.price*item.quantity);
         } catch (error) {
             console.log(error)
@@ -35,6 +38,19 @@ const CartItem = ({item, setTotalPrice, setChange}) => {
         }
     }
 
+    const updateItemHandler = async() => {
+        try {
+            const {data} = await axios.put(`user/cart/${auth.user.userID}/${item._id}`,{quantity: quantity})
+            setTotalPrice(prevPrice => prevPrice-product.price*item.quantity+product.price*quantity)
+            setChange(prevChange => !prevChange)
+            toast.success('Item updated!')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    const onChange = (value) => {
+        setQuantity(value);
+    };
     return (
         <div className='card mt-1 rounded'>
             <div className='row' style={{maxHeight: '12rem'}}>
@@ -45,9 +61,10 @@ const CartItem = ({item, setTotalPrice, setChange}) => {
                     <div className='d-flex justify-content-between'>
                         <div className='details'>
                             <h5>{product.name}</h5>
-                            <p className='text-muted'>Quantity: {item.quantity}</p>
+                            <p className='text-muted'>Quantity: <InputNumber min={1} max={product.quantity} value={quantity} onChange={(e) => onChange(e)}/></p>
                             <p className='text-info'>Price: ₹{product.price}</p>
                         </div>
+                        <button className="btn btn-info mb-5" style={{maxHeight: '40px'}} onClick={updateItemHandler} hidden={quantity == item.quantity}>Update</button>
                         <button className="btn btn-danger mb-5" style={{maxHeight: '40px'}} onClick={removeItemHandler}>Delete</button>
                     </div>
                 </div>
