872b9fc5d869dbc2dcc91697847f385fbf31f4b5
Raunak <raunakgiri21@gmail.com>
2023-09-14 10:50:05 +0530

    Merged Client into Server for deployment

--- client/src/pages/NotFound.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Jumbotron from "../components/cards/jumbotron";
-
-const NotFound = () => {
-    return <Jumbotron pageTitle='404' pageSubtitle='Page Not Found!'/>
-};
-
-export default NotFound
